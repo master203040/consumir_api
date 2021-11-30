8b703cdf0181e2169370fe4abfd445a20e3c1538
Carlos Alberto Ortega Caro <carlos.ortega@uam.edu.co>
2021-11-30 14:51:41 -0500

    proyecto api

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// components
+import Navbar from "./components/Navbar";
+import CharacterList from "./components/CharacterList";
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+  const url = "https://rickandmortyapi.com/api/character";
+
+  /*funsion de mostrar imagenes*/
+  const fetchCharacters = (url) => {
+    axios
+      .get(url)
+      .then((data) => {
+        setCharacters(data.data.results);
+        setInfo(data.data.info);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  /*funsion de mover boton Next*/
+  const handleNextPage = () => {
+    fetchCharacters(info.next);
+    window.scrollTo(0, 0);
+  };
+  /*funsion de mover boton Previous*/
+  const handlePreviousPage = () => {
+    fetchCharacters(info.prev);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    fetchCharacters(url);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container py-5">
+        <nav>
+          <ul className="pagination justify-content-center">
+            {info.prev ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handlePreviousPage}>
+                  Previous
+                </button>
+              </li>
+            ) : null}
+            {info.next ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handleNextPage}>
+                  Next
+                </button>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
+
+      <CharacterList characters={characters} />
+
+      <div className="container pb-5">
+        <nav>
+          <ul className="pagination justify-content-center">
+            {info.prev ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handlePreviousPage}>
+                  Previous
+                </button>
+              </li>
+            ) : null}
+            {info.next ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handleNextPage}>
+                  Next
+                </button>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
